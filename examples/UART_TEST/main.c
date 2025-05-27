e3923f8fcca767ca6118cda0ea1dc5e8c7fa8e47
emresagir <emresagir1310@gmail.com>
2025-05-27 17:52:20 +0300

    modified, UART_TEST to show RX functionality

--- examples/UART_TEST/main.c
+++ examples/UART_TEST/main.c
@@ -8,6 +8,20 @@ int main(void) {
     }
     UART_init();
     UART_printf("Hello, World!\n");
+    UART_printf("UART RX Polling started\n");
+
+    while(1){
+        unsigned int sr = UART0_STATE;
+        if(sr & (1<<21)){ //21 is Data reg full flag.
+
+            char c = (char)(UART0_DATA & 0xFF); //Taking the one byte data.
+
+            UART_printf("\n \n The value of the data = ");
+            char buf[2] = {c, '\0'}; 
+            UART_printf(buf);
+        }   
+    }
+    
     return 0;
     // UART_init();
     // UART_printf("Hello, World!\n");
