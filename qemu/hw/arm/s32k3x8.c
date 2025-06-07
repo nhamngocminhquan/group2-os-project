59f9519b415c330714034a07479ef4747ae26316
nhamngocminhquan <s342197@studenti.polito.it>
2025-06-07 11:47:14 +0200

    Create secondary CPU, add ELF parser to emulate SBAF
    Co-authored-by: nhamngocminhquan <nhamngocminhquan@gmail.com>

--- qemu/hw/arm/s32k3x8.c
+++ qemu/hw/arm/s32k3x8.c
@@ -1,4 +1,5 @@
 // Q:   A skeleton based on mps2.c and stm32f405_soc.c
+//      Also armsse.c and mps3r.c
 //      Common includes
 #include "hw/arm/s32k3x8.h"
 #include "qemu/osdep.h"
@@ -55,7 +56,43 @@ static void s32k3x8_init(MachineState *ms) {
     // Q:   Useful variables
     DeviceState *armv7m;
     MachineClass *mc = MACHINE_GET_CLASS(ms);
+
+    // Q:   SBAF variables
+    // Q:   According to Table 191 @ reference manual, the SBAF
+    // runs first on boot and will scan Flash program addresses
+    // 0x400000, 0x600000, 0x800000, 0xA00000 and data address
+    // 0x10000000 for the boot image vector table. The IVT, 
+    // whose format is defined in Table 200 @ refmanual, stores
+    // the addresses of vector tables of individual cores and
+    // the CPU_ENABLE bits in the boot configuration word.
+    // Our emulation here will only scan the ELF at location
+    // 0x400000, the first address in program flash.
+    // 
+    // We use the USE_SBAF define to set whether to emulate this
+    // functionality, by scanning the provided ELF for the boot
+    // image vector table. If USE_SBAF = 0 then these following
+    // default values will be used.
+    size_t cpu_enable[MAX_CPU] = {1, 1};
+    hwaddr ivt_addr[MAX_CPU] = {
+        0x00000000, 0x00000000
+    };
+#if USE_SBAF == 1
+    if (parse_elf_for_ivt(
+        ms->kernel_filename, ms->smp.cpus,
+        0b0101, cpu_enable, ivt_addr
+    ) < 0) {
+        exit(1);
+    }
+#endif
+
+    // Q:   This system memory is common to all CPUs.
+    //      It has the lowest priority (-1), so other private
+    //      memory regions can cover it. Since it can only be
+    //      added as subregions once, we need to create additional
+    //      aliases to add it to the CPUs. This is similar to
+    //      mps3r.c and armsse.c
     MemoryRegion *system_memory = get_system_memory();
+
     // Q:   Conversion to S32K3X8 to access members
     S32K3X8MachineState *sms = S32K3X8_MACHINE(ms);
 
@@ -73,39 +110,78 @@ static void s32k3x8_init(MachineState *ms) {
     // memory_region_init_ram(&sms->sram, NULL, "s32k3x8.sram", RAM_SIZE, &error_fatal);
     // memory_region_add_subregion(get_system_memory(), RAM_BASE_ADDRESS, &sms->sram);
 
-    // Q:   ITCM is needed for memory starting at 0, for the 
-    //      program/interrupt vector table
-    memory_region_init_ram(
-        &sms->itcm, NULL, "s32k3x8.itcm",
-        ITCM_SIZE, &error_fatal
-    );
-    memory_region_add_subregion(system_memory, ITCM_BASE_ADDRESS, &sms->itcm);
     memory_region_init_rom(
         &sms->pflash, NULL, "s32k3x8.flash",
         P_FLASH_SIZE, &error_fatal
     );
     memory_region_add_subregion(system_memory, P_FLASH_BASE_ADDRESS, &sms->pflash);
 
+    // Q:   Create CPU children
+    for (int cpu_i = 0; cpu_i < ms->smp.cpus; cpu_i++) {
+        // Create some CPU-specific names
+        g_autofree char *cpu_name = g_strdup_printf("armv7m-%d", cpu_i);
+        g_autofree char *cpu_mem_name = g_strdup_printf("armv7m-%d-memory", cpu_i);
+        g_autofree char *sys_mem_alias_name = g_strdup_printf("armv7m-%d-sysmem-alias", cpu_i);
+        g_autofree char *itcm_name = g_strdup_printf("armv7m-%d-itcm", cpu_i);
 
-    // Q:   Create CPU child
-    object_initialize_child(OBJECT(sms), "armv7m", &sms->armv7m, TYPE_ARMV7M);
-    armv7m = DEVICE(&sms->armv7m);
-    qdev_connect_clock_in(armv7m, "cpuclk", sms->sysclk);
-    qdev_connect_clock_in(armv7m, "refclk", sms->refclk);
-    qdev_prop_set_string(armv7m, "cpu-type", ms->cpu_type);
+        // Create CPU-specific memory region. We alias
+        // system_memory to add it to the CPUs as subregion
+        // with priority -1
+        memory_region_init(
+            &sms->cpu_memory[cpu_i], OBJECT(ms),
+            cpu_mem_name, UINT32_MAX
+        );
+        memory_region_init_alias(
+            &sms->sys_memory_alias[cpu_i], OBJECT(ms),
+            sys_mem_alias_name, system_memory, 0, UINT32_MAX
+        );
+        memory_region_add_subregion_overlap(
+            &sms->cpu_memory[cpu_i], 0,
+            &sms->sys_memory_alias[cpu_i], -1
+        );
 
-    // Q:   Set the starting addresses
-    // qdev_prop_set_uint32(armv7m, "init-svtor", P_FLASH_BASE_ADDRESS);
-    qdev_prop_set_uint32(armv7m, "init-nsvtor", P_FLASH_BASE_ADDRESS);
+        object_initialize_child(OBJECT(sms), cpu_name, &sms->armv7m[cpu_i], TYPE_ARMV7M);
+        armv7m = DEVICE(&sms->armv7m[cpu_i]);
+        qdev_connect_clock_in(armv7m, "cpuclk", sms->sysclk);
+        qdev_connect_clock_in(armv7m, "refclk", sms->refclk);
+        qdev_prop_set_string(armv7m, "cpu-type", ms->cpu_type);
 
-    // Q:   Unknowns
-    qdev_prop_set_uint32(armv7m, "num-irq", 256);
-    qdev_prop_set_bit(armv7m, "enable-bitband", true);
+        // Set the starting addresses
+        if (!cpu_enable[cpu_i])
+            qdev_prop_set_bit(armv7m, "start-powered-off", true);
 
-    // Q:   Connect CPU to memory
-    object_property_set_link(OBJECT(&sms->armv7m), "memory",
-                             OBJECT(system_memory), &error_abort);
-    sysbus_realize(SYS_BUS_DEVICE(&sms->armv7m), &error_fatal);
+        qdev_prop_set_uint32(armv7m, "init-svtor", ivt_addr[cpu_i]);
+        qdev_prop_set_uint32(armv7m, "init-nsvtor", ivt_addr[cpu_i]);
+
+        // IRQ-related properties
+        qdev_prop_set_uint32(armv7m, "num-irq", 256);
+        qdev_prop_set_uint8(armv7m, "num-prio-bits", 4);
+        qdev_prop_set_bit(armv7m, "enable-bitband", true);
+
+        // Connect CPU to memory
+        object_property_set_link(OBJECT(&sms->armv7m[cpu_i]), "memory",
+                                OBJECT(&sms->cpu_memory[cpu_i]), &error_abort);
+
+        // Realize the CPU
+        if (!sysbus_realize(SYS_BUS_DEVICE(&sms->armv7m[cpu_i]), &error_fatal)) {
+            error_report("Failed to realize CPU %d", cpu_i);
+            return;
+        }
+
+        // ITCM is needed for memory starting at 0. It is
+        // specific to each CPU
+        memory_region_init_ram(
+            &sms->itcm[cpu_i], NULL, itcm_name,
+            ITCM_SIZE, &error_fatal
+        );
+        memory_region_add_subregion(
+            &sms->cpu_memory[cpu_i], ITCM_BASE_ADDRESS,
+            &sms->itcm[cpu_i]
+        );
+    }
+
+    // Get reference to first CPU for peripherals
+    armv7m = DEVICE(&sms->armv7m[0]);
     
      //-------------- TIMER IMPLEMENTATION -----------------
     for (i = 0; i < NUM_TIMERS; i++) {
@@ -168,9 +244,15 @@ static void s32k3x8_init(MachineState *ms) {
     //                             0x40020000, 0x00010000);
 
     // Q:   Load kernel for simulation, size argument means kernel cannot
-    //      exceed this size? (probably cropped)
-    armv7m_load_kernel(sms->armv7m.cpu, ms->kernel_filename,
-                       0, RAM_SIZE);
+    //      exceed this size? (probably cropped). We call it twice because
+    //      each CPU needs to be reset to receive correctly the vector
+    //      table from init-nsvtor. If it is called only for the first CPU
+    //      the second CPU ignores init-nsvtor. The kernel is placed in
+    //      the CPU's view of the system, but if two CPU's have certain
+    //      similar components (such as Flash) the operation is redundant. 
+    for (int cpu_i = 0; cpu_i < ms->smp.cpus; cpu_i++)
+        armv7m_load_kernel(sms->armv7m[cpu_i].cpu, ms->kernel_filename,
+                        0, P_FLASH_SIZE);
 }
 
 static void s32k3x8_machine_init(MachineClass *mc) {
@@ -186,6 +268,7 @@ static void s32k3x8_machine_init(MachineClass *mc) {
     mc->default_ram_size = RAM_SIZE;
     // Q:   IMPORTANT: ID is crucial, otherwise QEMU will not create RAM
     mc->default_ram_id = "s32k3x8.ram";
+    mc->max_cpus = MAX_CPU;
     // mc->default_cpu_type = POWERPC_CPU_TYPE_NAME("755_v2.8");
     // mc->default_ram_size = CPIOM_MMAP_SDRAM_SIZE
 }
@@ -214,4 +297,106 @@ static void s32k3x8_machine_register_types(void)
     type_register_static(&s32k3x8_machine_typeinfo);
 }
 type_init(s32k3x8_machine_register_types)
- 
+
+// ====================================================
+//
+// Q:   Include for ELF file parsing. This is inspired
+// by include/hw/elf_ops.h.inc used by loader.c to load
+// the elf files
+#include <elf.h>
+
+// Note: cpu_mask denotes the cores that we will check
+// out of 4 possible cores. For example, the 358 has
+// cores 0 and 2, so the mask will be 0b0101.
+static ssize_t parse_elf_for_ivt(
+    const char* kernel_filename, int n_cpu,
+    size_t cpu_mask, size_t* cpu_enable, hwaddr* ivt_addr
+) {
+    int fd;
+    uint8_t e_ident[EI_NIDENT];
+
+    fd = open(kernel_filename, O_RDONLY | O_BINARY);
+    if (fd < 0) {
+        error_report("Could not load kernel '%s'", kernel_filename);
+        return -1;
+    }
+    if (read(fd, e_ident, EI_NIDENT) != EI_NIDENT) {
+        error_report("Kernel '%s': incorrect ELF identity size", kernel_filename);
+        goto fail;
+    }
+    if (e_ident[0] != ELFMAG0 ||
+        e_ident[1] != ELFMAG1 ||
+        e_ident[2] != ELFMAG2 ||
+        e_ident[3] != ELFMAG3) {
+        error_report("Kernel '%s': incorrect ELF magic", kernel_filename);
+        goto fail;
+    }
+    if (e_ident[EI_DATA] != ELFDATA2LSB) {
+        error_report("Kernel '%s': incorrect endianness", kernel_filename);
+        goto fail;
+    }
+    if (e_ident[EI_DATA] != ELFCLASS32) {
+        error_report("Kernel '%s': 64-bit not supported", kernel_filename);
+        goto fail;
+    }
+
+    Elf32_Ehdr elf_header;
+    lseek(fd, 0, SEEK_SET);
+    if (read(fd, &elf_header, sizeof(Elf32_Ehdr)) != sizeof(Elf32_Ehdr)) {
+        error_report("Kernel '%s': incorrect ELF header size", kernel_filename);
+        goto fail;
+    }
+
+    Elf32_Shdr section_header;
+    size_t found_ivt = 0;
+    uint32_t ivt_table[IVT_SIZE];
+    lseek(fd, elf_header.e_shoff, SEEK_SET);
+    for (int i = 0; i < elf_header.e_shnum; i++) {
+        lseek(fd, elf_header.e_shoff + 
+            i * sizeof(Elf32_Shdr), SEEK_SET);
+        if (read(fd, &section_header, sizeof(Elf32_Shdr)) != elf_header.e_shentsize) {
+            error_report("Kernel '%s': incorrect section header size", kernel_filename);
+            goto fail;
+        };
+        if (section_header.sh_addr == 0x400000) {
+            lseek(fd, section_header.sh_offset, SEEK_SET);
+            if (read(fd, &ivt_table, IVT_SIZE * sizeof(uint32_t)) != IVT_SIZE * sizeof(uint32_t)) {
+                error_report("Kernel '%s': incorrect boot IVT size", kernel_filename);
+                goto fail;
+            };
+            if (ivt_table[0] != 0x5AA55AA5) {
+                error_report("Kernel '%s': incorrect IVT marker", kernel_filename);
+                goto fail;
+            };
+            info_report("Found IVT");
+            found_ivt = 1;
+            break;
+        }
+    }
+    if (!found_ivt) {
+        error_report("Kernel '%s': could not find boot IVT", kernel_filename);
+        goto fail;
+    };
+
+    // Set CPU-enable bits and interrupt vector table addresses
+    int cpu = 0;
+    for (int i = 0; i < MAX_CPU; i++)
+        if (cpu_mask & (1 << i)) {
+            if (i < 3) {
+                cpu_enable[cpu] = (ivt_table[1] >> i) & 1;
+                ivt_addr[cpu] = ivt_table[3 + i * 2];
+            } else {
+                cpu_enable[cpu] = (ivt_table[1] >> 8) & 1;
+                ivt_addr[cpu] = ivt_table[10];
+            }
+            info_report("Set CPU %d with IVT %x", cpu, ivt_addr[cpu]);
+            cpu++;
+        }
+
+    close(fd);
+    return 0;
+
+fail:
+    close(fd);
+    return -1;
+}
--- qemu/include/hw/arm/s32k3x8.h
+++ qemu/include/hw/arm/s32k3x8.h
@@ -19,23 +19,28 @@
  #include "hw/timer/s32k3x8-timer.h"
 /* Main SYSCLK frequency in Hz */
 #define SYSCLK_FRQ 25000000
-// Q:   ARMV7M needs 2 clocks, sysclk and refclk. From mps2 this
-//      is the clock for the systick
+// ARMV7M needs 2 clocks, sysclk and refclk. From mps2 this
+// is the clock for the systick
 #define REFCLK_FRQ (1 * 1000 * 1000)
 
-// Q:   Values from memory map of reference manual
+// Multi-CPU variables
+#define MAX_CPU                     2
+#define USE_SBAF                    0
+#define IVT_SIZE                    11
+
+// Values from memory map of reference manual
 #define RAM_BASE_ADDRESS            0x20400000
 #define RAM_SIZE                    (768 * KiB)
 
-// Q:   Program flash: 4 separate blocks modeled as one
+// Program flash: 4 separate blocks modeled as one
 #define P_FLASH_BASE_ADDRESS        0x00400000
 #define P_FLASH_SIZE                (8192 * KiB)
 
 #define D_FLASH_BASE_ADDRESS        0x10020000
 #define D_FLASH_SIZE                (128 * KiB)
 
-#define ITCM_BASE_ADDRESS        0x00000000
-#define ITCM_SIZE                (64 * KiB)
+#define ITCM_BASE_ADDRESS           0x00000000
+#define ITCM_SIZE                   (64 * KiB)
 
 #define NUM_TIMERS 3
 
@@ -45,13 +50,18 @@ struct S32K3X8MachineClass {
 
 struct S32K3X8MachineState {
     MachineState parent;
-    ARMv7MState armv7m;
+    ARMv7MState armv7m[MAX_CPU];
     S32K3X8UARTState uart0;
+
+    // Local view of memory from each CPU
+    MemoryRegion cpu_memory[MAX_CPU];
+    // Aliases of system memory for each CPU
+    MemoryRegion sys_memory_alias[MAX_CPU];
 
     Clock *sysclk;
     Clock *refclk;
 
-    MemoryRegion itcm;
+    MemoryRegion itcm[MAX_CPU];
     MemoryRegion sram;  // Unused
     MemoryRegion pflash;
 
@@ -63,5 +73,11 @@ struct S32K3X8MachineState {
 // Q:   Since we have no virtual methods, SIMPLE_TYPE is enough
 // OBJECT_DECLARE_TYPE(S32K3X8MachineState, S32K3X8MachineClass, S32K3X8_MACHINE)
 OBJECT_DECLARE_SIMPLE_TYPE(S32K3X8MachineState, S32K3X8_MACHINE)
+
+// Q:   Prototype for parse_elf_for_ivt()
+static ssize_t parse_elf_for_ivt(
+    const char* kernel_filename, int n_cpu,
+    size_t cpu_mask, size_t* cpu_enable, hwaddr* ivt_addr
+);
 
 #endif
